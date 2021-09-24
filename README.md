e1b97be447325abf150bafeb41ab58a79ec83cfd
Jonathan Arnett <jonarnett90@gmail.com>
2021-09-24 03:26:37 -0400

    Allow user to pass file

--- README.md
+++ README.md
@@ -13,13 +13,15 @@ This is my first ever Rust project, so the code is probably really bad. Don't `@
 
 ## Running
 
-Run `cargo run`
+You have two options:
 
-You will be presented with a `jbi>` prompt. The state is retained across inputs to the REPL, so try to remember what the state of your array looks like.
+1. `cargo run`
+    You will be presented with a `jbi>` prompt. The state is retained across inputs to the REPL, so try to remember what the state of your array looks like.
+2. `cargo run <filename>`
+    The file specified will be executed by the JBI interpreter.
 
 ## TODO
 
 - [ ] Don't immediately give up if a loop is unclosed. Give the user the opportunity to close it.
-- [ ] Allow the user to pass a file to interpret instead of using the REPL.
 - [ ] Tests, probably.
 - [ ] J3RN's Brainf*ck Compiler (JBC) :wink:
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use annotate_snippets::{display_list::DisplayList, snippet::Snippet};
-use std::io::{self, Write};
+use std::env;
+use std::fs::File;
+use std::io::{self, Read, Write};
 
 mod lexer;
 mod runtime;
@@ -17,11 +19,37 @@ pub trait ErrorOutput {
 }
 
 fn main() {
-    let stdin = io::stdin();
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        1 => run_repl(),
+        2 => run_file(&args[1]),
+        _ => eprintln!(
+            r#"Usage: jbi [file]
+
+If no `file' is passed, you will be presented with a REPL. If `file' is given,
+it will be executed and output printed to the console."#
+        ),
+    }
+}
+
+fn run_file(filename: &str) {
+    let mut file = File::open(filename).expect("Could not open file");
     let mut state = runtime::State::default();
+    let mut content = String::new();
+
+    file.read_to_string(&mut content)
+        .expect("Failed to read file");
+
+    run(&mut state, &content, &filename)
+}
+
+fn run_repl() {
+    let stdin = io::stdin();
 
     loop {
         let mut input = String::new();
+        let mut state = runtime::State::default();
 
         print!("jbi> ");
         io::stdout().flush().unwrap();
@@ -33,27 +61,30 @@ fn main() {
             Ok(_) => (),
             Err(_) => break,
         }
+        run(&mut state, &input, "stdin")
+    }
+}
 
-        match lexer::lex(&input, "stdin") {
-            Ok(toks) => match semantic_analyzer::analyze(&toks) {
-                Ok(tree) => {
-                    if let Err(err) = runtime::eval(&mut state, &tree) {
-                        let dl = DisplayList::from(err.to_error());
-                        eprintln!("{}", dl);
-                    }
+fn run(state: &mut runtime::State, input: &str, filename: &str) {
+    match lexer::lex(input, filename) {
+        Ok(toks) => match semantic_analyzer::analyze(&toks) {
+            Ok(tree) => {
+                if let Err(err) = runtime::eval(state, &tree) {
+                    let dl = DisplayList::from(err.to_error());
+                    eprintln!("{}", dl);
                 }
-                Err(errs) => {
-                    for err in errs {
-                        let dl = DisplayList::from(err.to_error());
-                        eprintln!("{}", dl);
-                    }
-                }
-            },
+            }
             Err(errs) => {
                 for err in errs {
                     let dl = DisplayList::from(err.to_error());
                     eprintln!("{}", dl);
                 }
+            }
+        },
+        Err(errs) => {
+            for err in errs {
+                let dl = DisplayList::from(err.to_error());
+                eprintln!("{}", dl);
             }
         }
     }
